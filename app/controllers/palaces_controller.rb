62a6b2edd40337984b6cc6e622c04f21edc5ba3b
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-01 16:12:50 -0400

    adds create & strong params

--- app/controllers/palaces_controller.rb
+++ app/controllers/palaces_controller.rb
@@ -8,4 +8,15 @@ class PalacesController < ApplicationController
     palace = Palace.find_by(id: params[:id])
     render json: palace
   end
+
+  def create
+    # binding.pry
+    palace = Palace.create(palace_params)
+    render json: palace
+  end
+
+  def palace_params
+    params.require(:palace).permit(:name)
+  end
+
 end
