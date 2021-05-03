2200df450fde4858b0cee10b4ce821398c98b8ea
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-02 22:49:06 -0400

    adds create and params to loci

--- app/controllers/locis_controller.rb
+++ app/controllers/locis_controller.rb
@@ -1,2 +1,25 @@
 class LocisController < ApplicationController
+  def index
+    locis = Loci.all 
+    render json: locis
+  end
+
+  def create
+    # binding.pry
+    loci = Loci.create(loci_params)
+    render json: loci
+  end
+
+  def destroy
+    loci = Loci.find_by(id: params[:id])
+      # binding.pry
+    loci.item.destroy
+    loci.destroy
+    render json: {message: "did it"}
+  end
+
+  def loci_params
+    params.require(:loci).permit(:name, :item)
+  end
+
 end
--- app/controllers/palaces_controller.rb
+++ app/controllers/palaces_controller.rb
@@ -20,3 +20,4 @@ class PalacesController < ApplicationController
   end
 
 end
+ 
